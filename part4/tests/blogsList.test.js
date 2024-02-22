96b06232737a857e9d83fd0698d4cbf0cd7bfd99
ocm128 <odissea.2001@hotmail.com>
2024-02-22 19:18:03 +0100

    Exercises 4.10 - 4.11 completed

--- part4/tests/blogsList.test.js
+++ part4/tests/blogsList.test.js
@@ -1,6 +1,6 @@
 /*
   way to check a only test
-  npm test -- -t "HTTP or http"
+  npm test -- -t "HTTP or http or whatever"
 */
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -12,7 +12,6 @@ const iB = require('./initialBlogs')
 beforeEach(async () => {
 
   // Create blogs
-  //console.log(iB, 'BLOOOOOOGS')
   await Blog.deleteMany({})
 
   const blogObjects = iB.map(blog => new Blog(blog))
@@ -36,14 +35,61 @@ describe('HTTP id and not _id  ', () => {
   })
 })
 
-describe('HTTP id and not _id  ', () => {
-  test('The unique identifier property of the blog posts is by default _id', async () => {
-    const blogs = await Blog.find({})
-    expect(blogs[0]._id).toBeDefined()
+
+// Exercise 4.10
+describe('HTTP POST /api/blogs ', () => {
+  test('A new blog can be added ', async () => {
+
+    const newBlog = {
+      'title': 'UNIX in a nutshell',
+      'author': 'James C. Armstrong',
+      'url': 'https://unixinanutshell.com/',
+      'likes': 1018
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201) // Created
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(iB.length + 1)
+  })
+})
+
+// Exercise 4.11
+describe('Like property is missing and it will default to 0', () => {
+  test('http likes is missing', async () => {
+
+    const newBlog = {
+      'title': 'Javascript Ninja',
+      'author': 'John Resig',
+      'url': 'https://www.jsninja.com'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    console.log(response)
+
+    response.body.map(async (blog) => {
+      if (blog.likes === undefined) {
+        blog.likes = 0
+      }
+    })
+
+    response.body.map(async (blog) => {
+      await expect(blog.likes).toBeDefined()
+    })
+
   })
 })
 
 afterAll(() => {
   mongoose.connection.close()
 })
-
