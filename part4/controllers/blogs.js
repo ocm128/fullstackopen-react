974510d80f2082afb87deed73dd78f4ad9add00b
ocm128 <odissea.2001@hotmail.com>
2024-02-27 21:10:50 +0100

    Exercises 4.13 and 4.14 completed

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -19,4 +19,31 @@ blogsRouter.post('/', (request, response) => {
     })
 })
 
+// Exercise 4.13 (Blog list expansion)
+blogsRouter.delete('/:id', async(request, response, next) => {
+  const id = request.params.id
+  try {
+    await Blog.findByIdAndRemove(id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Exercise 4.14 (Blog list expansion)
+blogsRouter.put('/:id', async(request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+  
+  const blog = {
+    likes: body.likes
+  }
+  try {
+    const blogUpdated = await Blog.findByIdAndUpdate(id, blog, { new: true })
+    response.json(blogUpdated.toJSON())
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = blogsRouter
